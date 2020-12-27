a3a7977a1a429bd3c596bf1b546cd1b62ac7618b
Daniel <itzdanielmichael@gmail.com>
2020-12-27 10:39:13 -0500

    update render,actions,html

--- helpers/render.js
+++ helpers/render.js
@@ -3,10 +3,21 @@ function renderHabit(habit) {
   return `
 		<li class='habit ${habit.checked ? "checked" : ""}' data-id='${habit.id}'>
 			<habit-card>
-				<input class='checkbox' type='checkbox' ${habit.checked ? "checked" : ""}>
-        <h1 class='name'>${habit.name}</h1>&nbsp;${deleteHabitButton(
-    habit.checked
-  )}
+        <input class='checkbox' type='checkbox' ${
+          habit.checked ? "checked" : ""
+        }>
+
+        <input id="edit-input-field-${
+          habit.id
+        }" class='edit-text inactive' type='text' >
+
+        <h1 class='name' id="habit-name-${habit.id}">${
+    habit.name
+  }</h1>&nbsp;${deleteHabitButton(habit.checked)}
+  <button onclick="editHabit('${habit.id}')">Edit</button>
+  <button onclick="saveHabit('${habit.id}')">Save</button>
+  <button onclick="cancelEdit('${habit.id}')">Cancel</button>
+
         <!-- <h1 class='date'>${habit.date}</h1>&nbsp -->
 			</habit-card>
 		</li>
